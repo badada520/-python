8d7a6aa567f957b3936041bb61be8743645cfbda
badada520 <3468332113@qq.com>
2025-10-28 13:00:34 +0800

    Add files via upload

---
+++ student_account.sql
@@ -0,0 +1,7 @@
+INSERT INTO student_portal_student (account, name, role, is_active)
+VALUES
+('20250001','张三','student',1),
+('20250002','李四','student',1),
+('T2025001','王老师','teacher',1),
+('22920242203360','潘奕怀','student',1),
+('22920242203216','方锦轩','student',1);
